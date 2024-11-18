194318f54f7aa29a7fab412082947990c0b8a6e8
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-11-19 00:31:42 +0100

    Remove "ported" processor

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,7 +1,3 @@
 from .elf import *
 from .asm import *
 from .utils import *
-
-from .asm.processor import run
-
-__all__ = ['run']
--- processor/asm/__init__.py
+++ processor/asm/__init__.py
@@ -1,5 +1,4 @@
 from .block import GlobalAsmBlock
 from .function import Function
-from .processor import parse_source
 
-__all__ = ['GlobalAsmBlock', 'Function', 'parse_source']
+__all__ = ['GlobalAsmBlock', 'Function']
