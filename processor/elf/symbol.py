5db7b206e15ebabb8cf6f709b4ca6b2e4c9773b4
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-11-19 01:17:58 +0100

    Fix types

--- processor/elf/symbol.py
+++ processor/elf/symbol.py
@@ -1,7 +1,6 @@
 from typing import Optional
 from ..utils.constants import SHN_XINDEX
 from .format import ElfFormat
-from .section import Section
 
 class Symbol:
     """
@@ -15,7 +14,7 @@ class Symbol:
     } Elf32_Sym;
     """
 
-    def __init__(self, fmt: ElfFormat, data: bytes, strtab: Section, name: Optional[str]=None) -> None:
+    def __init__(self, fmt: ElfFormat, data: bytes, strtab: "Section", name: Optional[str]=None) -> None:
         self.fmt = fmt
         self.st_name, self.st_value, self.st_size, st_info, self.st_other, self.st_shndx = fmt.unpack('IIIBBH', data)
         assert self.st_shndx != SHN_XINDEX, "too many sections (SHN_XINDEX not supported)"
@@ -25,7 +24,7 @@ class Symbol:
         self.visibility = self.st_other & 3
 
     @staticmethod
-    def from_parts(fmt: ElfFormat, st_name: int, st_value: int, st_size: int, st_info: int, st_other: int, st_shndx: int, strtab: Section, name: str) -> "Symbol":
+    def from_parts(fmt: ElfFormat, st_name: int, st_value: int, st_size: int, st_info: int, st_other: int, st_shndx: int, strtab: "Section", name: str) -> "Symbol":
         header = fmt.pack('IIIBBH', st_name, st_value, st_size, st_info, st_other, st_shndx)
         return Symbol(fmt, header, strtab, name)
 
